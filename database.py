29bbcddc9fb76755846cb3d102f42335b01bc2fa
TanweerAdnan <adnantanweer83@gmail.com>
2026-05-12 20:53:09 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    email TEXT UNIQUE,
+    password TEXT
+)
+""")
+
+conn.commit()
